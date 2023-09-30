001e68c06535232dfd9109522399e79a61f005f4
Shou9.123.Myko <mikofernando2003@gmail.com>
2023-09-30 09:25:54 +0700

    Template Front End Project PSI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Nanti Ngoding disini, Jalanin nya lu ketik `npm install` tunggu ampe
+        kelar, trus lu ketik lagi `npm run dev`,
+      </h1>
+      <h1>Folder component jangan di hapus itu buat component nya nanti</h1>
+    </div>
+  );
+}
+
+export default App;
